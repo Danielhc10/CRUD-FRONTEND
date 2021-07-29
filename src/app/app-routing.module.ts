669321f4c2176c75c4fb52879a7bb5359c3e20ba
Daniel Hernandez <dani2203g@gmail.com>
2021-07-28 21:56:48 -0500

    AVANCE 2: NUEVOS COMPONENTES

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { AboutusComponent } from './principal/aboutus/aboutus.component';
+import { HomeComponent } from './principal/home/home.component';
+import { LoginComponent } from './principal/login/login.component';
+import { RegisterComponent } from './principal/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+    { path: 'about', component: AboutusComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'footer', component: FooterComponent },
+    { path: '**', component: HomeComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './principal/home/home.component';
+import { AboutusComponent } from './principal/aboutus/aboutus.component';
+import { RegisterComponent } from './principal/register/register.component';
+import { LoginComponent } from './principal/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    AboutusComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
